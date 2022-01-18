adbf4cf66516b4f980c799304ceb70b41a691b48
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2022-01-19 01:53:02 +0200

    pdo work

--- likeImgFunc.php
+++ likeImgFunc.php
@@ -1,15 +1,27 @@
 <?php
-  function imgLookup($currentImg) {
-    $dir = "./images";
-    $imgArray = scandir($dir);
-    $imgArrayLength = count($imgArray);
-    $randomNum = rand(2, $imgArrayLength - 1);
-    $imgUrl = $imgArray[$randomNum];
-    if ($imgUrl !== $currentImg) return $imgUrl;
-    else return imgLookup($currentImg);
+  function likeImg($likedImg) {
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+    
+    try {
+      $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "INSERT INTO Likes (imgName) VALUES ($likedImg)";
+      $conn->exec($sql);
+      echo json_encode('Successfully liked image.');
+    } catch(PDOException $e) {
+      echo json_encode($sql . " - " . $e->getMessage());
+    }
+    
+    $conn = null;
   }
 
-  $response = imgLookup($_REQUEST["currentImg"]);
+  $response = likeImg($_REQUEST["likedImg"]);
 
   echo json_encode($response);
 ?>
